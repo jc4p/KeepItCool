8563959c53722addf65ec53321b1918fed0e7330
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2015-05-30 21:51:58 -0400

    Initial Commit

---
+++ KeepItCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeepItCool
+//
+//  Created by Kasra Rahjerdi on 5/30/15.
+//  Copyright (c) 2015 Kasra Rahjerdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
