e913789513f6042e425c1e51958a86de4d77c622
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2015-06-04 21:06:02 -0400

    Shouldn't have commited these, apparently

--- Pods/Target Support Files/Pods-KeepItCool-APAddressBook/Pods-KeepItCool-APAddressBook-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KeepItCool_APAddressBook : NSObject
-@end
-@implementation PodsDummy_Pods_KeepItCool_APAddressBook
-@end
--- Pods/Target Support Files/Pods-KeepItCool-CryptoSwift/Pods-KeepItCool-CryptoSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KeepItCool_CryptoSwift : NSObject
-@end
-@implementation PodsDummy_Pods_KeepItCool_CryptoSwift
-@end
